d9a2e9d567111aaaf754a6ace701c6145f1e676d
Danielle Vaitsman <d734051@gmail.com>
2021-04-01 14:03:20 +0300

    first change - testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './components/Home';
+import SignIn from './components/SignIn';
+import './style.css';
+import './importStyle.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component  {
+  constructor (props) {
+    super(props);
+    this.state = {
+      userID: 0,
+      language: 'english',
+
+    }
+  }
+  
+  render(){
+    if (this.state.userID !== 0){
+      return (
+        <Home
+          user={this.state.userID} 
+          logOut={()=>{this.setState({userID:0})}}
+          language = {this.state.language}
+        />
+      );
+    } else {
+      return (
+        <SignIn
+          validLogin ={(userid) => {this.setState({userID: userid})}}
+          changeLanguage = {(update)=>{this.setState({language: update})}}
+        />
+      );
+    }
+  }
 }
-
-export default App;
---
+++ src/components/SIgnIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class SignIn extends React.Component {
+    render (){
+        return (
+            <div>
+                <h1>signin page</h1>
+            </div>
+        );
+    }
+}
